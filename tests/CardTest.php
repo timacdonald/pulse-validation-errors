8debcd3535ff9bb125aaca57fc45e0da1e0933c9
Tim MacDonald <hello@timacdonald.me>
2024-05-15 07:23:10 +1000

    Fixes tests

--- tests/CardTest.php
+++ tests/CardTest.php
@@ -25,10 +25,17 @@ it('renders', function () {
                 method: 'POST',
                 uri: '/register',
                 action: 'App\Http\Controllers\RegisterController@store',
-                bag: 'default',
+                bag: null,
                 name: 'email',
                 message: null,
                 count: 1,
+                key_hash: md5(json_encode([
+                    'POST',
+                    '/register',
+                    'App\Http\Controllers\RegisterController@store',
+                    'default',
+                    'email',
+                ])),
             ));
 
             return true;
@@ -57,10 +64,59 @@ it('optionally supports', function () {
                 method: 'POST',
                 uri: '/register',
                 action: 'App\Http\Controllers\RegisterController@store',
-                bag: 'default',
+                bag: null,
                 name: 'email',
                 message: 'The email field is required.',
                 count: 1,
+                key_hash: md5(json_encode([
+                    'POST',
+                    '/register',
+                    'App\Http\Controllers\RegisterController@store',
+                    'default',
+                    'email',
+                    'The email field is required.',
+                ])),
+            ));
+
+            return true;
+        });
+});
+
+it('supports error bags', function () {
+    Pulse::record(
+        type: 'validation_error',
+        key: json_encode([
+            'POST',
+            '/register',
+            'App\Http\Controllers\RegisterController@store',
+            'custom_1',
+            'email',
+            'The email field is required.',
+        ], flags: JSON_THROW_ON_ERROR),
+    )->count();
+    Pulse::ingest();
+
+    Livewire::test(ValidationErrors::class, ['lazy' => false])
+        ->assertViewHas('errors', function ($errors) {
+
+            expect($errors)->toHaveCount(1);
+
+            expect($errors[0])->toEqual(literal(
+                method: 'POST',
+                uri: '/register',
+                action: 'App\Http\Controllers\RegisterController@store',
+                bag: 'custom_1',
+                name: 'email',
+                message: 'The email field is required.',
+                count: 1,
+                key_hash: md5(json_encode([
+                    'POST',
+                    '/register',
+                    'App\Http\Controllers\RegisterController@store',
+                    'custom_1',
+                    'email',
+                    'The email field is required.',
+                ])),
             ));
 
             return true;
